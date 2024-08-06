d89cb0acea45358ff0ed14f53b393430df85ed6f
Arshpreet Singh Sandhu <sanda09@pfw.edu>
2024-08-06 01:35:34 -0400

    UI setup

--- inventory-management-system/app/page.js
+++ inventory-management-system/app/page.js
@@ -2,12 +2,12 @@
 import Image from "next/image"
 import {useState, useEffect} from 'react'
 import {firestore} from '@/firebase'
-import {Box, Typography} from '@mui/material'
+import {Box, Modal, Typography, Stack, TextField} from '@mui/material'
 import {collection, getDocs , query} from 'firebase/firestore'
 
 export default function Home() {
  const [inventory, setInventory] = useState([])
- const [open, setOpen] = useState(false)
+ const [open, setOpen] = useState(true)
  const [itemName, setItemName] = useState([''])
 
 const updateInventory = async () =>{
@@ -61,17 +61,47 @@ const handleOpen = () => setOpen(true)
 const handleClose = () => setOpen(false)
 
 return(
-  <Box>
+  <Box
+  width="100vw"
+  height="100vh"
+  display="flex"
+  justifyContent="center"
+  alignItems="center"
+  gap={2}
+  >
+   <Modal open={open} onClose={handleClose}>
+    <Box
+    position="absolute"
+    top="50%"
+    left="50%"
+    width={400}
+    bgcolor="white"
+    border="2px solid #000"
+    boxShadow={24}
+    p={4}
+    display="flex"
+    flexDirection="column"
+    gap={3}
+    sx={{
+      tranform: 'translate(-50%, -50%)',
+    }}
+    >
+      <Typography variant="h6">Add Item</Typography>
+      <Stack width="100%" direction="row" spacing={2}>
+          <TextField
+          variant="outlined"
+          fullWidth
+          value={itemName}
+          onChange={(e) =>{
+            setItemName(e.target.value)
+          }}
+          />
+          
+      </Stack>
+    </Box>
+   </Modal>
+
     <Typography variant="h1">Inventory Management</Typography>
-    {inventory.forEach((item)=>{
-      console.log(item)
-      return(
-        <Box>
-          {item.name}
-          {item.count}
-        </Box>
-      )
-    })}
   </Box>
 )
 
